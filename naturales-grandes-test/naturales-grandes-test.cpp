a04b66b45a555ea57e24242381c927c9e5e72bda
Miguel Latre <latre@unizar.es>
2019-11-13 13:40:57 +0100

    Refactorización del código que escribe los resultados de las pruebas
    Añadidas funciones para la escritura de los resultados de las pruebas para cadenas de caracteres, booleanos y enteros.
    Se utilizan también secuencias de escape para escribir los mensajes de error en rojo.

--- naturales-grandes-test/naturales-grandes-test.cpp
+++ naturales-grandes-test/naturales-grandes-test.cpp
@@ -24,6 +24,16 @@ using namespace std;
 const int MAX_DIGITOS = 30;
 const char CADENA_NO_INICIALIZADA[MAX_DIGITOS] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
+
+/* Funciones que escriben los resultados de las pruebas.
+ * Por legibilidad, aquí solo se declaran. La definición completa de la función
+ * está al final del fichero.
+ */
+void escribirResultadoPrueba(const char valorEsperado[], 
+                             const char valorCalculado[]);
+void escribirResultadoPrueba(const int valorEsperado, const int valorCalculado);
+void escribirResultadoPrueba(const bool valorEsperado, const bool valorCalculado);
+
 /*
  * Pre:  «valorEsperado» es la secuencia de dígitos de «n» en base 10, terminado
  *       en '\0'.
@@ -37,14 +47,7 @@ void probarConvertir(const int n, const char valorEsperado[]) {
     char valorCalculado[MAX_DIGITOS];
     strcpy(valorCalculado, CADENA_NO_INICIALIZADA);
     convertir(n, valorCalculado);
-    if (strcmp(valorEsperado, valorCalculado) == 0) {
-        cout << "correcta." << endl;
-    }
-    else {
-        cout << "incorrecta:" << endl;
-        cout << "   debería haber calculado \"" << valorEsperado
-             << "\" pero ha calculado \"" << valorCalculado << "\"." << endl;
-    }
+    escribirResultadoPrueba(valorEsperado, valorCalculado);
 }
 
 /*
@@ -60,14 +63,7 @@ void probarSumar(const char a[], const char b[], const char valorEsperado[]) {
     char valorCalculado[MAX_DIGITOS];
     strcpy(valorCalculado, CADENA_NO_INICIALIZADA);
     sumar(a, b, valorCalculado);
-    if (strcmp(valorEsperado, valorCalculado) == 0) {
-        cout << "correcta." << endl;
-    }
-    else {
-        cout << "incorrecta:" << endl;
-        cout << "   debería haber calculado \"" << valorEsperado
-             << "\" pero ha calculado \"" << valorCalculado << "\"." << endl;
-    }
+    escribirResultadoPrueba(valorEsperado, valorCalculado);
 }
 
 
@@ -82,15 +78,8 @@ void probarSumar(const char a[], const char b[], const char valorEsperado[]) {
  */
 void probarValor(const char numero[], const int valorEsperado) {
    cout << "Prueba valor(\"" << numero << "\") ";
-   int valorDevuelto =    valor(numero);
-   if (valorDevuelto == valorEsperado) {
-      cout << "correcta." << endl;
-   }
-   else {
-      cout << "incorrecta:" << endl;
-      cout << "   debería haber devuelto " << valorEsperado
-           << " pero ha devuelto " << valorDevuelto << '.' << endl;
-   }
+   int valorDevuelto = valor(numero);
+   escribirResultadoPrueba(valorDevuelto, valorEsperado);
 }
 
 
@@ -108,14 +97,7 @@ void probarCalcularImagen(const char numero[], const char valorEsperado[]) {
     char valorCalculado[MAX_DIGITOS];
     strcpy(valorCalculado, CADENA_NO_INICIALIZADA);
     calcularImagen(numero, valorCalculado);
-    if (strcmp(valorEsperado, valorCalculado) == 0) {
-        cout << "correcta." << endl;
-    }
-    else {
-        cout << "incorrecta:" << endl;
-        cout << "   debería haber calculado \"" << valorEsperado
-             << "\" pero ha calculado \"" << valorCalculado << "\"." << endl;
-    }
+    escribirResultadoPrueba(valorEsperado, valorCalculado);
 }
 
 /*
@@ -130,12 +112,79 @@ void probarCalcularImagen(const char numero[], const char valorEsperado[]) {
 void probarEsCapicua(const char numero[], const bool valorEsperado) {
     cout << "Prueba esCapicua (\"" << numero << "\") ";
     bool valorCalculado = esCapicua(numero);
+    escribirResultadoPrueba(valorEsperado, valorCalculado);
+}
+
+/********************************************************************************/
+const char ESCAPE_ROJO[] = "\033[31m";
+const char ESCAPE_ROJO_NEGRITA[] = "\033[31;1m";
+const char ESCAPE_NORMAL[] = "\033[0m";
+
+/*
+ * Pre:  En la línea actual de la pantalla se ha escrito un texto anunciando que
+ *       se ha realizado una prueba de una determinada función con unos
+ *       determinados argumentos.
+ * Post: Si los valores de «valorEsperado» y «valorCalculado» son iguales, ha
+ *       completado un mensaje en la pantalla informando de que la prueba ha sido
+ *       correcta. En caso contrario, indica que ha sido incorrecta y ha informado
+ *       también de los valores de «valorEsperado» y «valorCalculado».
+ */
+void escribirResultadoPrueba(const char valorEsperado[], 
+                             const char valorCalculado[]) {
+    if (strcmp(valorEsperado, valorCalculado) == 0) {
+        cout << "correcta." << endl;
+    }
+    else {
+        cout << "incorrecta:" << endl;
+        cout << ESCAPE_ROJO;
+        cout << "   debería haber calculado \"" << valorEsperado
+             << "\" pero ha calculado \"" << valorCalculado << "\"." << endl;
+        cout << ESCAPE_NORMAL;
+    }
+}
+
+/*
+ * Pre:  En la línea actual de la pantalla se ha escrito un texto anunciando que
+ *       se ha realizado una prueba de una determinada función con unos
+ *       determinados argumentos.
+ * Post: Si los valores de «valorEsperado» y «valorCalculado» son iguales, ha
+ *       completado un mensaje en la pantalla informando de que la prueba ha sido
+ *       correcta. En caso contrario, indica que ha sido incorrecta y ha informado
+ *       también de los valores de «valorEsperado» y «valorCalculado».
+ */
+void escribirResultadoPrueba(const int valorEsperado, const int valorCalculado) {
     if (valorEsperado == valorCalculado) {
         cout << "correcta." << endl;
     }
     else {
         cout << "incorrecta:" << endl;
-        cout << "   debería haber calculado \"" << boolalpha << valorEsperado
-             << "\" pero ha calculado \"" << valorCalculado << "\"." << endl;
+        cout << ESCAPE_ROJO;
+        cout << "   debería haber calculado " << valorEsperado
+             << " pero ha calculado " << valorCalculado << '.' << endl;
+        cout << ESCAPE_NORMAL;
+    }
+}
+
+/*
+ * Pre:  En la línea actual de la pantalla se ha escrito un texto anunciando que
+ *       se ha realizado una prueba de una determinada función con unos
+ *       determinados argumentos.
+ * Post: Si los valores de «valorEsperado» y «valorCalculado» son iguales, ha
+ *       completado un mensaje en la pantalla informando de que la prueba ha sido
+ *       correcta. En caso contrario, indica que ha sido incorrecta y ha informado
+ *       también de los valores de «valorEsperado» y «valorCalculado».
+ */
+void escribirResultadoPrueba(const bool valorEsperado, const bool valorCalculado) {
+    if (valorEsperado == valorCalculado) {
+        cout << "correcta." << endl;
+    }
+    else {
+        cout << "incorrecta:" << endl;
+        cout << ESCAPE_ROJO << boolalpha;
+        cout << "   debería haber calculado " << ESCAPE_ROJO_NEGRITA 
+             << valorEsperado << ESCAPE_ROJO << " pero ha calculado "
+             << ESCAPE_ROJO_NEGRITA << valorCalculado << ESCAPE_ROJO << '.' 
+             << endl;
+        cout << ESCAPE_NORMAL;
     }
 }
